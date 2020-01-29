004048bd5bfc35b5d6f85f8ff945162c60c4f50e
noel <noelseo@gmail.com>
2020-01-28 17:42:28 -0800

    Build Data and Save Image Icons

---
+++ src/data.js
@@ -0,0 +1,128 @@
+//color = category
+//size = company name and value
+
+const data = [
+  {
+    cat: "Financial Services",
+    name: "JPMorgan",
+    value: 88,
+    icon: "../img/jpmorgan.png",
+    desc: `Software Engineer`
+  },
+  {
+    cat: "Education",
+    name: "UCSF",
+    value: 13,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Education",
+    name: "UC Berkeley",
+    value: 20,
+    icon: "../img/berkeley.png",
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Education",
+    name: "UC Davis",
+    value: 20,
+    icon: "../img/ucdavis.png",
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Tech",
+    name: "Google",
+    value: 100,
+    icon: "../img/google.png",
+    desc: 'Software Engineer'
+  },
+  {
+    cat: "Tech",
+    name: "Salesforce",
+    value: 70,
+    icon: "../img/salesforce.png",
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Biotech",
+    name: "Genentech",
+    value: 14,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Healthcare",
+    name: "United Healthcare",
+    value: 30,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Biotech",
+    name: "Neuralink",
+    value: 40,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Financial Services",
+    name: "Capital One",
+    value: 80,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Education",
+    name: "App Academy",
+    value: 8,
+    desc: "Instructor"
+  },
+  {
+    cat: "Tech",
+    name: "Microsoft",
+    value: 150,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Tech",
+    name: "Apple",
+    value: 180,
+    icon: "../img/apple.png",
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Retail",
+    name: "Nordstrom",
+    value: 30,
+    desc: "Frontend Engineer"
+  },
+  {
+    cat: "Nonprofit",
+    name: "Bill & Melinda Gates Foundation",
+    value: 10,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Automotive",
+    name: "Tesla",
+    value: 65,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Nonprofit", 
+    name: "American Heart Association",
+    value: 10,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Financial Services",
+    name: "Visa",
+    value: 20,
+    desc: "Software Engineer"
+  },
+  {
+    cat: "Tech",
+    name: "Facebook",
+    value: 100,
+    icon: "../img/fb.png",
+    desc: "Software Engineer"
+  }
+];
+
+module.exports = data;

8444c9b06f13b7dc743f2b54f2ea0f6e155c625c
noel <noelseo@gmail.com>
2020-01-30 11:24:34 -0800

    Add More/Less Openings Legend

--- src/index.js
+++ src/index.js
@@ -169,6 +169,31 @@ function run() {
     .style("font-size", "20px") 
     .call(legendOrdinal);
   
+  // --------------------------------------------------------------------------------
+  // less openings, more openings
+
+  let sizeScale = d3
+    .scaleOrdinal()
+    .domain(["More openings", "Less openings"])
+    .range([20, 10]);
+
+  let legendSize = d3
+    .legendSize()
+    .scale(sizeScale)
+    .shape("circle")
+    .labelAlign("end");
+
+  let legend2 = svg
+    .append("g")
+    .classed("legend-size", true)
+    .attr("text-anchor", "start")
+    .attr("transform", "translate(100, 400)") //left num = x axis, right num = y axis
+    .style("font-size", "20px")
+    .call(legendSize);
+
+  // ---------------------------------------------------------------------------------
+
+ 
 }
 
 main.innerHTML = run();
